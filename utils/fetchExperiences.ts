a08892dc31e4c17ebb9eddc24ff72cfac88f34e8
Neelesh Chandwani <neeleshchandwani@gmail.com>
2022-10-31 15:16:22 +0530

    Resolve Type error

--- utils/fetchExperiences.ts
+++ utils/fetchExperiences.ts
@@ -6,7 +6,7 @@ export const fetchExperiences = async () => {
   );
 
   const data = await res.json();
-  const experiences: Experience = data.experiences;
+  const experiences: Experience[] = data.experiences;
 
   return experiences;
 };
